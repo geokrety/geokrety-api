5e0e3725e730deb3dd0a12c70ebdc9765f8856ec
Mathieu Alorent <github@kumy.net>
2017-09-02 01:04:09 +0200

    [CI] check utilities

--- app/api/helpers/utilities.py
+++ app/api/helpers/utilities.py
@@ -1,4 +1,11 @@
 
+from app.api.helpers.exceptions import UnprocessableEntity
 
 def dasherize(text):
     return text.replace('_', '-')
+
+def require_relationship(resource_list, data):
+    for resource in resource_list:
+        if resource not in data:
+            raise UnprocessableEntity({'pointer': '/data/relationships/{}'.format(resource)},
+                "A valid relationship with {} resource is required".format(resource))
